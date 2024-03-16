21947a89000270e7e8922a6c1da280301fb18ccf
westhecool <52859033+westhecool@users.noreply.github.com>
2024-03-16 12:19:16 -0400

    upload

---
+++ README.md
@@ -0,0 +1,10 @@
+Not complete yet
+<hr>
+A video downloader that uses puppeteer to bypass antibot protections.
+<br><br>
+
+```sh
+usage: node index.js <url> [<output_file>]
+```
+
+Supported domains: embtaku.pro, binged.in
---
+++ index.js
@@ -0,0 +1,124 @@
+
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
+puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
+const cp = require('child_process');
+function sanitizeFilename(filename) {
+    // Windows filename restrictions:
+    // Reserved characters: < > : " / \ | ? *
+    // Reserved names: CON, PRN, AUX, NUL, COM1, COM2, COM3, COM4, COM5, COM6, COM7, COM8, COM9, LPT1, LPT2, LPT3, LPT4, LPT5, LPT6, LPT7, LPT8, LPT9
+    // Also, filenames cannot end with a space or a period.
+
+    const reservedNames = /^(con|prn|aux|nul|com[1-9]|lpt[1-9])$/i;
+    const sanitized = filename
+        .replace(/[<>:"/\\|?*]+/g, '')  // Remove reserved characters
+        .replace(/\.\s*$/, '')          // Remove trailing period
+        .replace(/\s+$/, '');           // Remove trailing spaces
+
+    // Check if the name is a reserved name after removing the invalid characters
+    if (reservedNames.test(sanitized)) {
+        return `${sanitized}_safe`; // Append something to make it safe
+    }
+
+    return sanitized;
+}
+var index_of_m3u8 = 1, pbrowser = null, page = null, got = false;
+const start = async () => {
+    pbrowser = await puppeteer.launch({ headless: true });
+    page = await pbrowser.newPage();
+    await page.setRequestInterception(true);
+    let i = 0;
+    page.on('request', interceptedRequest => {
+        if (interceptedRequest.url().includes('m3u8')) {
+            i++;
+            if (i == index_of_m3u8) {
+                console.log('got the m3u8 request!');
+                got = true;
+                headers = '';
+                for (let key in interceptedRequest.headers()) {
+                    headers += key + ': ' + interceptedRequest.headers()[key] + '\r\n';
+                }
+                const p = cp.spawn('ffmpeg', ['-loglevel', 'error', '-stats', '-protocol_whitelist', 'file,crypto,data,https,http,tls,tcp', '-headers', headers, '-i', interceptedRequest.url(), '-c:v', 'copy', '-c:a', 'copy', output_file, '-y'], { stdio: 'inherit' });
+                p.on('exit', (code) => {
+                    if (code !== 0) {
+                        console.log('ffmpeg exited with code ' + code);
+                        console.log('failed to download video');
+                        process.exit(1);
+                    } else {
+                        console.log('ffmpeg exited with code ' + code);
+                        console.log('successfully downloaded video to ' + output_file);
+                        process.exit(0);
+                    }
+                });
+                pbrowser.close();
+            }
+        }
+        interceptedRequest.continue();
+    });
+}
+const binged_in = async () => {
+    const go = async () => {
+        await page.goto(url);
+        setTimeout(async () => {
+            if (!got) {
+                console.log('timeout reached, retrying...');
+                go();
+            }
+        }, 3000);
+    }
+    go();
+}
+const embtaku_pro = async () => {
+    await page.goto(url);
+    if (!url.includes('/streaming.php')) {
+        console.log('attempting to get the streaming url...');
+        const element = await page.$('iframe');
+        url = await page.evaluate((element) => element.src, element);
+        console.log('got the streaming url: ' + url);
+        await page.goto(url);
+    }
+    await page.waitForSelector('div[aria-label="Play"]');
+    const element = await page.$('div[aria-label="Play"]');
+    const click = async () => { // try multiple times to click on the element because we will trigger popups
+        try {
+            await page.bringToFront();
+            const boundingBox = await element.boundingBox();
+            await page.mouse.move(boundingBox.x + boundingBox.width / 2, boundingBox.y + boundingBox.height / 2);
+            await page.mouse.down();
+            await page.mouse.up();
+        } catch (e) {}
+        setTimeout(async () => {
+            if (!got) click();
+        }, 500);
+    }
+    click();
+}
+var output_file = process.argv[3];
+var url = process.argv[2];
+if (!url) {
+    console.log('usage: node index.js <url> [<output_file>]');
+    process.exit(1);
+}
+if (!output_file) {
+    output_file = sanitizeFilename(url.replace('https://', '').replace('http://', '').replace(/\//g, '_')) + '.mp4';
+}
+switch ((new URL(url)).host) {
+    case 'embtaku.pro':
+        start().then(() => {
+            index_of_m3u8 = 1;
+            embtaku_pro();
+        });
+        break;
+    case 'binged.in':
+        start().then(() => {
+            index_of_m3u8 = 2;
+            binged_in();
+        });
+        break;
+    default:
+        console.log('domain not supported ' + (new URL(url)).host);
+        console.log('supported domains: embtaku.pro, binged.in');
+        process.exit(1);
+}
